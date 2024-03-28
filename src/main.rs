a0a62bafb1fda6ad3ce20e3be5f01a01fd8aaa3e
Alan Sparrow <git@freeflight.org.uk>
2024-03-28 10:27:31 +0000

    Load YAXIM

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
+use gloo::net::http::Request;
 use leptos::*;
 
+use yaixm::Yaixm;
+
+mod yaixm;
+
+#[component]
+fn App() -> impl IntoView {
+    let async_data = create_local_resource(|| (), |_| async move { fetch_yaixm().await });
+
+    view! {
+        { move || match async_data.get() {
+            Some(resource) => match resource {
+                Some(yaixm) => view! { <p>"Release date: "{ yaixm.release.airac_date }</p> },
+                None => view! {<p>"Error loading YAXIM"</p> }
+            }
+            None => view! { <p>"Loading YAIXM..."</p> }
+        }}
+    }
+}
+
+// Get YAIXM data from server
+async fn fetch_yaixm() -> Option<Yaixm> {
+    let result = Request::get("yaixm.json").send().await;
+    match result {
+        Ok(response) => response.json().await.ok(),
+        _ => None,
+    }
+}
+
 fn main() {
-    mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    mount_to_body(|| view! { <App/> })
 }
