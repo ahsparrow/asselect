d4da79e25e85acdbbebc51608bc6dc3d3984f3ee
Alan Sparrow <git@freeflight.org.uk>
2024-03-30 17:13:23 +0000

    Download file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use gloo::file::{Blob, ObjectUrl};
 use gloo::net::http::Request;
 use leptos::*;
 
@@ -51,7 +52,7 @@ fn MainView(yaixm: Yaixm) -> impl IntoView {
                         .map(|n| view! {
                             <div class="field">
                             <label class="checkbox">
-                            <input type="checkbox" class="mr-2"/>
+                            <input name={n.clone()} type="checkbox" class="mr-2"/>
                             {n}
                             </label>
                             </div>
@@ -59,6 +60,24 @@ fn MainView(yaixm: Yaixm) -> impl IntoView {
                         .collect_view()
                     }
                 </ul>
+            </div>
+
+            <div class="container block">
+                <div class="mx-4">
+                <button type="submit" class="button is-primary"
+                    on:click = |_| {
+                        let blob = Blob::new("Hello Alan");
+                        let object_url = ObjectUrl::from(blob);
+
+                        let a = leptos::html::a();
+                        a.set_download("openair.txt");
+                        a.set_href(&object_url);
+                        a.click();
+                    }
+                >
+                    {"Get Airspace"}
+                </button>
+                </div>
             </div>
         </div>
     }
