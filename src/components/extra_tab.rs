0ed0eb5f01e1f3374ae26f135832ec8648901192
Alan Sparrow <git@freeflight.org.uk>
2024-04-10 18:40:37 +0100

    Hide clear button on inactive extra panels

--- src/components/extra_tab.rs
+++ src/components/extra_tab.rs
@@ -6,21 +6,28 @@ use crate::settings::{ExtraType, Settings};
 pub fn ExtraTab(children: Children, names: Vec<String>, ids: Vec<ExtraType>) -> impl IntoView {
     let setter = use_context::<WriteSignal<Settings>>().expect("to find setter");
 
+    let (get, set) = create_signal(0);
+
     names
         .iter()
         .zip(children().nodes.iter())
         .zip(ids)
-        .map(|((name, child), id)| {
+        .enumerate()
+        .map(|(n, ((name, child), id))| {
             view! {
                 <div class="card block">
-                    <header class="card-header">
+                    <header class="card-header is-clickable" on:click=move |_| set(n)>
                         <p class="card-header-title">{ name }</p>
-                        <div class="card-header-icon">
-                            <input class="button is-info is-light is-small ml-2" type="button" value="Clear" on:click=move |_| setter.update(|s| s.clear_extra(id)) />
+                        <div hidden=move || get()!=n>
+                            <div class="card-header-icon">
+                                <input class="button is-info is-light is-small ml-2"
+                                    type="button" value="Clear"
+                                    on:click=move |_| setter.update(|s| s.clear_extra(id)) />
+                            </div>
                         </div>
                     </header>
 
-                    <div class="card-content">
+                    <div class="card-content" hidden=move || get()!=n>
                         {child}
                     </div>
                 </div>
