0bb06600d0bfe463bea839f8256341db942bcc31
Alan Sparrow <git@freeflight.org.uk>
2024-04-16 14:30:17 +0100

    NOTAM tab contents

--- src/components/notam_tab.rs
+++ src/components/notam_tab.rs
@@ -2,5 +2,34 @@ use leptos::*;
 
 #[component]
 pub fn NotamTab() -> impl IntoView {
-    view! { <p>"Notam"</p> }
+    view! {
+        <div>
+            <div class="subtitle">
+                "Navigation Warnings"
+            </div>
+            <div class="block">
+                "The PDFs below show a summary of the navigation warning NOTAMs relevant to cross country gliding. The PDFs are refreshed during the day at approximately ten minutes to the hour.}
+            </div>
+            <div class="block ml-4">
+                <a href="https://navplot.asselect.uk/today_south.pdf" download={""}>
+                    "Download Today (England/Wales) PDF"
+                </a>
+            </div>
+            <div class="block ml-4">
+                <a href="https://navplot.asselect.uk/today_north.pdf" download={""}>
+                    "Download Today (North England/Scotland) PDF"
+                </a>
+            </div>
+            <div class="block ml-4">
+                <a href="https://navplot.asselect.uk/tomorrow_south.pdf" download={""}>
+                    "Download Tomorrow (England/Wales) PDF"
+                </a>
+            </div>
+            <div class="block ml-4">
+                <a href="https://navplot.asselect.uk/tomorrow_north.pdf" download={""}>
+                    "Download Tomorrow (North England/Scotland) PDF"
+                </a>
+            </div>
+        </div>
+    }
 }
